6b7b174243dc64863d61c683a80be939fa95a21e
Lukas Forst <lukas@forst.pw>
2020-04-08 00:52:54 +0200

    styles for home

--- app/renderer/components/Home.js
+++ app/renderer/components/Home.js
@@ -1,40 +1,47 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../../../dist-assets/routes';
+import 'materialize-css';
+import { Collection, CollectionItem } from 'react-materialize';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container">
         <h2>Home</h2>
-        <ul>
-          <li>
-            <Link to={routes.EMPLOYEES}>to Employees</Link>
-          </li>
-          <li>
-            <Link to={routes.WORK_TYPES}>to Work Types</Link>
-          </li>
 
-          <li>
-            <Link to={routes.CLIENTS}>to Clients</Link>
-          </li>
+        <div id="links">
+          <Collection>
+            <CollectionItem href="#">
+              <Link to={routes.EMPLOYEES}>to Employees</Link>
+            </CollectionItem>
 
-          <li>
-            <Link to={routes.ORDERS}>Orders</Link>
-          </li>
+            <CollectionItem href="#">
+              <Link to={routes.WORK_TYPES}>to Work Types</Link>
+            </CollectionItem>
 
-          <li>
-            <Link to={routes.EMPLOYEES_REPORTS}>Employee Reports</Link>
-          </li>
+            <CollectionItem href="#">
+              <Link to={routes.CLIENTS}>to Clients</Link>
+            </CollectionItem>
 
-          <li>
-            <Link to={routes.ORDER_REPORTS}>Orders Reports</Link>
-          </li>
+            <CollectionItem href="#">
+              <Link to={routes.ORDERS}>Orders</Link>
+            </CollectionItem>
 
-          <li>
-            <Link to={routes.SAVE_STATE}>Save work</Link>
-          </li>
-        </ul>
+            <CollectionItem href="#">
+              <Link to={routes.EMPLOYEES_REPORTS}>Employee Reports</Link>
+            </CollectionItem>
+
+            <CollectionItem href="#">
+              <Link to={routes.ORDER_REPORTS}>Orders Reports</Link>
+            </CollectionItem>
+
+            <CollectionItem href="#">
+              <Link to={routes.SAVE_STATE}>Save work</Link>
+            </CollectionItem>
+          </Collection>
+        </div>
+
       </div>
     );
   }
