49d4bf98c40714781be7c3b44069c2201a1b0941
Lukas Forst <lukas@forst.pw>
2020-04-08 00:45:09 +0200

    add styles for save page

--- app/renderer/components/tools/Save.js
+++ app/renderer/components/tools/Save.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import BackButton from './BackButton';
 import { remote } from 'electron';
 import * as storage from 'electron-json-storage';
+import { Button, Icon, TextInput } from 'react-materialize';
+import 'materialize-css';
 
 export default class Save extends Component {
 
@@ -31,20 +33,44 @@ export default class Save extends Component {
     return (
       <div>
         <BackButton/>
-        <div>
-          {path}
+        <div id="save-page">
+          <div id="stored-path">
+            <TextInput
+              disabled
+              icon={<Icon>folder</Icon>}
+              value={path}
+            />
+          </div>
+
+          <div id="path-buttons">
+            <Button
+              className="red"
+              node="button"
+              waves="light"
+              onClick={() => saveState(state, path)}
+            >
+              Save state
+            </Button>
+
+            <Button
+              className="red"
+              node="button"
+              waves="light"
+              onClick={() => loadState(changeState, path)}
+            >
+              Load state
+            </Button>
+
+            <Button
+              className="red"
+              node="button"
+              waves="light"
+              onClick={() => this.setPath()}
+            >
+              Open
+            </Button>
+          </div>
         </div>
-        <button type="button" onClick={() => saveState(state, path)}>
-          Save state.
-        </button>
-
-        <button type="button" onClick={() => loadState(changeState, path)}>
-          Load state
-        </button>
-
-        <button type="button" onClick={() => this.setPath()}>
-          Open
-        </button>
       </div>
     );
   }
